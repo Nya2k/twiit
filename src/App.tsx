ed954d552d952a5644fe35d9550e6e8c9874bf86
Nya2k <eudoravanya@gmail.com>
2024-03-18 23:45:43 +0700

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { IoPersonCircleSharp } from "react-icons/io5";
+
 export default function Header() {
   return (
     <div className="text-white text-xl font-serif font-semibold">
-      <header className="bg-[#161A30] p-4 flex">
+      <header className="bg-[#161A30] p-4 flex items-center">
         <div className='w-1/6 flex'>
-          <p className='ml-3'>logo</p>
           <p className='items-start tracking-tight hover:tracking-wide w-fit ml-3'>twiit</p>
         </div>
-        <div className='w-5/6 flex justify-end'>
-          <p>logo</p>
-          <p className='ml-4 mr-3'>username</p>
+        <div className='w-5/6 flex justify-end items-center'>
+          <IoPersonCircleSharp className="object-contain size-9"/>
+          <p className='ml-2 mr-3'>username</p>
         </div>
       </header>
     </div>
--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -1,3 +1,8 @@
+import { BsFillPeopleFill } from "react-icons/bs";
+import { FaPaperPlane } from "react-icons/fa6";
+import { FaTrashAlt } from "react-icons/fa";
+import { IoPersonCircleSharp } from "react-icons/io5";
+
 export default function Timeline() {
   return(
     <div className="bg-[#161A30] h-screen text-white flex justify-center">
@@ -8,27 +13,27 @@ export default function Timeline() {
         </div>
         <div className="flex mt-5 h-1/4">
           <input id="username" type="text" placeholder="What's happening?" className='flex w-2/3 h-full border-[#7FC7D9] shadow-[#7FC7D9] shadow-sm border-2 rounded-lg mr-3 bg-[#161A30] p-4 placeholder-white placeholder:-translate-y-14 text-lg'></input>
-          <div className="flex-col items-end w-1/3 h-full text-[#161A30] text-lg font-bold">
+          <div className="flex flex-col justify-end w-1/3 h-full text-[#161A30] text-lg font-bold">
             <div className="kotak1 bg-white shadow-white shadow-md border-2 rounded-lg h-12 flex justify-center items-center p-2">
-              <p className="mr-2">Edit Close Friends</p>
-              <p>logo</p>
+              <p className="">Edit Close Friends</p>
+              <BsFillPeopleFill className="ml-2"/>
             </div>
             <div className="kotak2 bg-white shadow-white shadow-md border-2 rounded-lg h-12 flex justify-center items-center mt-4">
-              <p className="mr-2">Post</p>
-              <p>logo</p>
+              <p>Post</p>
+              <FaPaperPlane className="ml-2"/>
             </div>
           </div>
         </div>
         
         <div className="container flex-col mt-6 p-5 px-7 bg-gradient-to-b from-[#2a3d4d] via-[#1f3445] shadow-[#2a3d4d] shadow-lg rounded-lg text-white h-fit">
           <div className="header flex">
-            <div className="flex justify-start w-5/6 ">
-              <p className="mr-3">profile</p>
-              <p className="mr-3">username</p>
-              <p>04-02-2024</p>
+            <div className="flex justify-start items-center w-5/6 ">
+              <IoPersonCircleSharp className="object-contain size-9 mr-2"/>
+              <p className="mr-3 font-bold text-lg">username</p>
+              <p className="text-xs">04-02-2024</p>
             </div>
             <div className="flex justify-end w-1/6">
-              <p>logo</p>
+            <FaTrashAlt/>
             </div>
           </div>
           <div className="mt-3">
@@ -36,21 +41,7 @@ export default function Timeline() {
           </div>
         </div>
 
-        <div className="container flex-col mt-6 p-5 px-7 bg-gradient-to-b from-[#2a3d4d] via-[#1f3445] shadow-[#2a3d4d] shadow-lg rounded-lg text-white h-fit">
-          <div className="header flex">
-            <div className="flex justify-start w-5/6 ">
-              <p className="mr-3">profile</p>
-              <p className="mr-3">username</p>
-              <p>04-02-2024</p>
-            </div>
-            <div className="flex justify-end w-1/6">
-              <p>logo</p>
-            </div>
-          </div>
-          <div className="mt-3">
-            <p>test</p>
-          </div>
-        </div>
+        
 
       </div>
     </div>
